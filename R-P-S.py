4bc6f2edfb80e1bf3d92aa23fc954b4bbf44df26
GITperson <79372651+nothing-hub@users.noreply.github.com>
2021-03-29 02:59:55 -0700

    Update R-P-S.py
    its a new update with new options

--- R-P-S.py
+++ R-P-S.py
@@ -7,6 +7,7 @@ print("scissors......")
 
 
 randomNumber = random.randint(0, 2)
+computerMove = "rock"
 if randomNumber == 0:
     computerMove = "rock"
 elif randomNumber == 1:
@@ -14,35 +15,50 @@ elif randomNumber == 1:
 elif randomNumber == 2:
     computerMove = "scissors"
 conpyterMove = "rock"
-Player_1 = input('player_1 make your move : ').lower()
-print(f'player_2 make your move : {computerMove}')
-Player_2 = computerMove
+player1_wins = 0
+player2_wins = 0
+wining_score = 4
+while player1_wins < wining_score and player2_wins < wining_score:
 
-if Player_1 == Player_2:
-    print("the result is tie \n equal")
-if Player_1 == "rock":
-    if Player_2 == "scissors":
-        print("player_1 win the game")
-    elif Player_2 == "paper":
-        print("player_2 win the game")
+    print('----------------------------------------------------------------')
+    print(f"player1 wins : {player1_wins} and player2 wins : {player2_wins}")
+    print('----------------------------------------------------------------')
+    Player_1 = input('player_1 make your move : ').lower()
+    print(f'player_2 make your move : {computerMove}')
+    Player_2 = computerMove
+    if Player_1 == "q" or Player_1 == "quit":
+        break
+    if Player_1 == Player_2:
+        print("the result is tie \n equal")
+    if Player_1 == "rock":
+        if Player_2 == "scissors":
+            print("player_1 win the game")
+            player1_wins += 1
+        elif Player_2 == "paper":
+            print("player_2 win the game")
+            player2_wins += 1
+    elif Player_1 == "paper":
+        if Player_2 == "rock":
+            print("player_1 win the game")
+            player1_wins += 1
+        elif Player_2 == "scissors":
+            print("player_2 win the game")
+            player2_wins += 1
+    elif Player_1 == "scissors":
+        if Player_2 == "paper":
+            print("player_1 win the game")
+            player1_wins += 1
+        elif Player_2 == "rock":
+            print("player_2 win the game")
+            player2_wins += 1
+    else:
+        print("something went wrong!")
 
 
-elif Player_1 == "paper":
-    if Player_2 == "rock":
-        print("player_1 win the game")
-    elif Player_2 == "scissors":
-        print("player_2 win the game")
+
+print(f"final score ===>  player1 wins : {player1_wins} and player2 wins : {player2_wins}")
 
 
-elif Player_1 == "scissors":
-    if Player_2 == "paper":
-        print("player_1 win the game")
-    elif Player_2 == "rock":
-        print("player_2 win the game")
-
-
-else:
-    print("something went wrong!")
 
 
 
